34262fe68431532996b24933106857401e6ad5e4
Sadegh Biglar <it2@shabake.abhar>
2026-08-30 19:09:47 +0330

    add student component

---
+++ resources/views/components/⚡students.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+       public string $title = 'دانش‌آموزان آموزشگاه';
+};
+?>
+
+<div>
+  <h1>{{ $title }}</h1>
+
+    <p>این اولین کامپوننت Livewire پروژه ماست </p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\StudentController;
+//use App\Http\Controllers\StudentController;
 
-Route::get('/students', [StudentController::class, 'index']);
+//Route::get('/students', [StudentController::class, 'index']);
+
+
+Route::livewire('/students', 'students');
