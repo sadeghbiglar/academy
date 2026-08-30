c10fd0ac524c948b127ded92fda112ee62841a42
Sadegh Biglar <it2@shabake.abhar>
2026-08-30 19:20:35 +0330

    install mary ui

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-//use App\Http\Controllers\StudentController;
-
-//Route::get('/students', [StudentController::class, 'index']);
-
-
-Route::livewire('/students', 'students');
+Route::livewire('/', 'pages::users.index');
